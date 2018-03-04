37e36373cccd256fb0de5832b6ed00122ab268d8
tanshuai <tanshuai@sobey.com>
2018-03-04 17:26:23 +0800

    连表

--- kaka-app-mservice/src/main/java/com/sobey/KakaAppMserviceApplication.java
+++ kaka-app-mservice/src/main/java/com/sobey/KakaAppMserviceApplication.java
@@ -1,9 +1,11 @@
 package com.sobey;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class KakaAppMserviceApplication {
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@SpringBootApplication
+@EnableEurekaClient
+public class KakaAppMserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KakaAppMserviceApplication.class, args);
