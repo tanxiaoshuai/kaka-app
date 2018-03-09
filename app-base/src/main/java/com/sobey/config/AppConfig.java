b45b6bb35d09452295b96566322a92f76a45c5e0
tanshuai <Pass2word>
2018-03-09 19:17:47 +0800

    新增接口

--- app-base/src/main/java/com/sobey/config/AppConfig.java
+++ app-base/src/main/java/com/sobey/config/AppConfig.java
@@ -16,8 +16,46 @@ public class AppConfig {
     public static final String AL_SMS_ACCESSKEYID = "LTAIhkCX6XUdl6R9";
     public static final String AL_SMS_ACCESSKEYSECRET = "ydA5bVkdWVbNj0aARlGcP2yhJ84CEy";
     public static final String AL_SMS_SIGN = "谭帅";
-    public static final String AL_SMS_REGISTE_TEMPLATECODE= "SMS_126875012";
-    public static final String AL_SMS_UPDATE_PWD_TEMPLATECODE= "SMS_126875012";
+    public static final String TEMPLATECODE= "SMS_126875012";
     public static final Long AL_SMS_OUTTIME = 600L;
     public static final Integer AL_SMS_MESSAGE_CODE_SIZE = 6;
+
+    enum Al_SMS_TEMPLATECODE{
+        REGISTE(1 , "SMS_126875012"),
+        UPDATE_PWD(2 , "SMS_126875012")
+        ;
+        private static Al_SMS_TEMPLATECODE sms = null;
+        private Integer key;
+        private String val;
+
+        Al_SMS_TEMPLATECODE(Integer key, String val) {
+            this.key = key;
+            this.val = val;
+        }
+//
+//        private Al_SMS_TEMPLATECODE(){}
+//        public static Al_SMS_TEMPLATECODE getSms(){
+//            if(sms == null){
+//               sms = new Al_SMS_TEMPLATECODE();
+//            }
+//        }
+
+        public Integer getKey() {
+            return key;
+        }
+
+        public void setKey(Integer key) {
+            this.key = key;
+        }
+
+        public String getVal() {
+            return val;
+        }
+
+        public void setVal(String val) {
+            this.val = val;
+        }
+
+
+    }
 }
