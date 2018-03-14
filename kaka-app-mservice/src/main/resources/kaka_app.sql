e1df8ba4bcdfe4e13a50cfc96ca6a5ce1632b3c6
tanshuai <Pass2word>
2018-03-14 18:57:15 +0800

    新增接口

--- kaka-app-mservice/src/main/resources/kaka_app.sql
+++ kaka-app-mservice/src/main/resources/kaka_app.sql
@@ -133,3 +133,8 @@ insert  into `t_user_site`(`userid`,`siteid`,`jobid`,`departmentid`) values ('2a
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+
+
+
+
